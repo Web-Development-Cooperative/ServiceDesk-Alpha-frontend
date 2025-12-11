9aed85ced2d8ff8dd7ca5d233030c6f2202ec95d
Emmanuil <emmanuil.litvinenko@inbox.ru>
2025-12-11 23:17:50 +0500

    feat: add the ability to change and delete a branch from the branch card, add caching, add a redirect by condition

--- src/features/branchDisplay/lib/useBranchDisplay.ts
+++ src/features/branchDisplay/lib/useBranchDisplay.ts
@@ -1,14 +1,26 @@
 import { useEffect } from 'react';
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
 
 import { mapBranch, useLazyGetBranchByIdQuery } from '~~>entities/branch';
+import { ROUTES } from '~~>shared/model/routes';
 
 const useBranchDisplay = () => {
 	const { 'branch-id': branchId } = useParams();
-	const [triger, { data }] = useLazyGetBranchByIdQuery();
+	const navigate = useNavigate();
+	const [trigger, { data }] = useLazyGetBranchByIdQuery();
 
 	useEffect(() => {
-		triger(branchId || '');
+		const fetch = async () => {
+			try {
+				await trigger(branchId || '', true).unwrap();
+			} catch (error) {
+				const err = error as { status: number };
+				if ([400, 404, 500].includes(err.status))
+					navigate(ROUTES.BRANCHES);
+			}
+		};
+
+		fetch();
 	}, [branchId]);
 
 	return { data: data ? mapBranch(data) : undefined };
